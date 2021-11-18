fbc1084e9621e480052a29a209e4bcaf6d75bcf2
unknown <israaaburayya2012@gmail.com>
2021-11-18 19:45:04 +0200

    handle script file RELATES#4

---
+++ script.js
@@ -0,0 +1,17 @@
+const app=document.getElementById('app')
+const cards=document.createElement('div')
+cards.setAttribute('class','cards')
+app.appendChild(cards)
+
+fetch('https://fakestoreapi.com/products')
+.then(res =>res.json())
+.then(data => data.forEach(element => {
+        cards.innerHTML +=`  <div class="product">
+        <img src="${element.image}"/>
+        <h2>${element.title}</h2>
+        <p>${element.description}</p>
+        <span class="price">${element.price}$</span>
+        <button>Add to Card</button>
+        </div>`
+}))
+.catch(err => alert(err))
