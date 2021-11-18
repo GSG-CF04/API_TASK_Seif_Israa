80a1fe30c33ab301634f47a2cca3efaef15baa63
kakashii7 <speed20112@gmail.com>
2021-11-18 20:52:56 +0200

    add heading with js Realtes #9

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
 const app=document.getElementById('app')
+const heading = document.createElement('h1')
+heading.textContent = 'Shopping Store'
+app.appendChild(heading)
 const cards=document.createElement('div')
 cards.setAttribute('class','cards')
 app.appendChild(cards)
+
 
 fetch('https://fakestoreapi.com/products')
 .then(res =>res.json())
