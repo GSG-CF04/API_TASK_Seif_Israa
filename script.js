19b2219fe7d9f81d67bd3ebaa4010cfaa693d4f6
unknown <israaaburayya2012@gmail.com>
2021-11-20 15:44:34 +0200

    add javascript code for shop card RELATES#12

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ const app=document.getElementById('app')
 const heading = document.createElement('h1')
 heading.textContent = 'Shopping Store'
 app.appendChild(heading)
+const shopping=document.createElement('div')
+shopping.setAttribute('class','shopping')
+app.appendChild(shopping)
+shopping.innerHTML = `  <button type="button" id="shopping" class="btn-shop"><i class="fas fa-shopping-cart"></i></button>
+<div class="product-shop">
+    <table>
+        <thead>
+            <tr>
+                <th>Quantity</th>
+                <th>price</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    <hr>
+    <div class="total">
+        <span>Total</span>
+        <span id="total">0$</span>
+    </div>
+</div>`
+
+let shoppingIcon=document.getElementById('shopping')
+let procutShop=document.querySelector('.product-shop')
+shoppingIcon.addEventListener('click',()=>{     
+   procutShop.classList.toggle('blocked')
+})
 const cards=document.createElement('div')
 cards.setAttribute('class','cards')
 app.appendChild(cards)
