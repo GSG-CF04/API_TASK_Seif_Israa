b4e23d049f9dc622545620d2d968827872914a6e
unknown <israaaburayya2012@gmail.com>
2021-11-21 12:58:26 +0200

    add script function for product type RELATES#19

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const app=document.getElementById('app')
 const heading = document.createElement('h1')
 heading.textContent = 'Shopping Store'
 app.appendChild(heading)
+const categories=document.createElement('div')
+categories.setAttribute('class','categories')
+categories.innerHTML =`<button data-class="all" class="cat-type active">All</button>`
+app.appendChild(categories)
 const shopping=document.createElement('div')
 shopping.setAttribute('class','shopping')
 app.appendChild(shopping)
@@ -34,21 +38,74 @@ shoppingIcon.addEventListener('click',()=>{
 const cards=document.createElement('div')
 cards.setAttribute('class','cards')
 app.appendChild(cards)
-
-
+fetch('https://fakestoreapi.com/products/categories')
+.then(res =>res.json())
+.then(data => data.forEach(element => {
+  categories.innerHTML +=`<button data-class="${element}" class="cat-type">${element}</button>`
+  let prodType=document.querySelectorAll('.categories button')
+for (let i=0 ;i<prodType.length;i++){
+  prodType[i].onclick = function(){                
+    // remove class from sibling
+    var el = prodType[0];
+    while(el)
+    {
+        if(el.tagName === "BUTTON"){
+            //remove class
+            el.classList.remove("active");   
+        }
+        // pass to the new sibling
+        el = el.nextSibling;
+    }
+  this.classList.add("active"); 
+  let dataClass=prodType[i].getAttribute('data-class')
+  let prod=document.querySelectorAll('.product')
+  if (dataClass === 'all') {
+          prod.forEach(ele => {ele.style.display='block'})
+    } 
+    else {
+      prod.forEach(ele => { ele.style.display='none'})
+      console.log(dataClass)
+      if (dataClass =="men's clothing") {
+        dataClass="men"
+      }
+      if (dataClass =="women's clothing") {
+        dataClass="women"
+      }
+      let dataClassElement=document.querySelectorAll(`.${dataClass}`)
+      dataClassElement.forEach(ele => { ele.style.display='block'})
+    } 
+};
+}
+}))
+.catch(err => alert(err))
 fetch('https://fakestoreapi.com/products')
 .then(res =>res.json())
 .then(data => data.forEach(element => {
-        cards.innerHTML +=`  <div class="product">
+  if (element.category =="men's clothing") {
+    cards.innerHTML +=`  <div class="product men">
         <img src="${element.image}"/>
         <h2>${element.title}</h2>
-        <p>${element.description}</p>
         <span class="price">${element.price}$</span>
         <button class="add-to-card" onclick="addToProductShop(${element.price},${element.id})">Add to Card</button>
         </div>`
+  }
+  else if (element.category =="women's clothing") {
+    cards.innerHTML +=`<div class="product women">
+        <img src="${element.image}"/>
+        <h2>${element.title}</h2>
+        <span class="price">${element.price}$</span>
+        <button class="add-to-card" onclick="addToProductShop(${element.price},${element.id})">Add to Card</button>
+        </div>`
+  }else {
+        cards.innerHTML +=`<div class="product ${element.category}">
+        <img src="${element.image}"/>
+        <h2>${element.title}</h2>
+        <span class="price">${element.price}$</span>
+        <button class="add-to-card" onclick="addToProductShop(${element.price},${element.id})">Add to Card</button>
+        </div>`
+      }
 }))
 .catch(err => alert(err))
-
  let tbody=document.querySelector('tbody')
 const  addToProductShop = (price,id) => {
         console.log(5)
@@ -103,20 +160,17 @@ btn.onclick = function topFunction() {
     document.documentElement.scrollTop = 0; 
   };
 btn.id = "myBtn";
-
-
 document.body.appendChild(btn);
-
 mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
+
 
